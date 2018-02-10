eef3bb7fc3ef6f6a71b8bde1adfffd9da8a615e6
Cameron Rollheiser <icirellik@gmail.com>
2018-02-10 16:26:43 -0500

    Indians Colors
    Use the colors for the indians.

--- lib/components/Faq.jsx
+++ lib/components/Faq.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FullWidthSection from './FullWidthSection';
-import { deepRed, offWhite, rubyRed } from './colors';
+import { deepRed, heroText, offWhite, greyBlue } from './colors';
 
 const styles = {
   container: {
@@ -12,14 +12,14 @@ const styles = {
     overflow: 'hidden',
   },
   h1: {
-    color: deepRed,
+    color: offWhite,
     fontFamily: 'Play, serif',
     fontSize: '50px',
     fontWeight: '600',
     lineHeight: '1.08',
     marginBottom: '40px',
     marginTop: '20px',
-    textShadow: `2px 2px 5px ${rubyRed}`,
+    textShadow: `2px 2px 5px ${greyBlue}`,
     textTransform: 'uppercase',
   },
   buttonMain: {
@@ -30,6 +30,12 @@ const styles = {
   buttonMainLabel: {
     lineHeight: '45px',
   },
+  p: {
+    color: heroText,
+    fontSize: '23px',
+    fontWeight: '400',
+    lineHeight: '1.58',
+  },
 };
 
 const Faq = () => (
@@ -37,23 +43,23 @@ const Faq = () => (
     <section style={styles.container}>
       <div>
         <h1 style={styles.h1}>Welcome to the CLE Tech Survey</h1>
-        <p>
+        <p style={styles.p}>
           Popular tech surveys don’t seem to get Cleveland. So, we made our own
           Cleveland-based survey to try to get a more accurate idea of what goes on
           in Cleveland tech.
         </p>
-        <p>
+        <p style={styles.p}>
           The purpose of this survey is to collect and present information about
           salary, education, and experience for those in Cleveland Technology to
           get an understanding of the influence different components have on
           salary calculations.
         </p>
-        <p>
+        <p style={styles.p}>
           The information collected is anonymous and will be accessible to all,
           as a downloadable csv, once the survey collection period is over
           March 31st, 2018.
         </p>
-        <p>
+        <p style={styles.p}>
           The survey takes about 7 minutes to complete.
         </p>
       </div>
